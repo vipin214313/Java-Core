386dbeec8e5793e92f657f9b1c453b9f27a7245a
vipin214313 <vipin214313pri@gmail.com>
2026-03-28 08:26:18 +0530

    java practical examples

---
+++ nonPri.java
@@ -0,0 +1,7 @@
+public class nonPri {
+    public static void main(String[] args) {
+        String s="simplilernas";  //string a sequence of charaters
+        System.out.println(s);
+
+    }
+}
---
+++ primitive.java
@@ -0,0 +1,31 @@
+public class primitive {
+
+    public static void main(String[] args) {
+        //integre values
+        int a=30; 
+        short c=20;
+        long l2=1200;
+        byte b=30;
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(l2);
+        System.out.println(b);
+
+        //dcimal types
+        float f=30.30f;
+        double d2=1556.124d;
+
+        System.out.println(f);
+        System.out.println(d2);
+
+        //charatcetrs
+        char charter2='A';
+        System.out.println(charter2);
+
+        //booleN TYPES
+        boolean isActive=true;
+        System.out.println(isActive);
+
+
+    }
+}

e8d030b60195c7d05e066a4472ab09949f262f24
vipin214313 <vipin214313pri@gmail.com>
2026-03-31 08:34:25 +0530

    java practical examples

---
+++ whileex.java
@@ -0,0 +1,9 @@
+public class whileex {
+
+    public static void main(String[] args) {
+        int i=0;
+        while(i == 0){
+            System.out.println("Hellow words");
+        }
+    }
+}
---
+++ whileloop.java
@@ -0,0 +1,11 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=5){
+            System.out.println("hellow java..");
+
+            i++;
+            System.out.println(i);
+        }
+    }
+}
